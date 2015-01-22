335af2df0584fa969e4aba49912e770ef6c4035e
wenfeiliang <nebulaliang@gmail.com>
2015-01-22 00:09:06 -0500

    update 2015/01/22

--- src/firstMissingPositive/FirstMissingPositive.java
+++ src/firstMissingPositive/FirstMissingPositive.java
@@ -2,55 +2,34 @@ package firstMissingPositive;
 
 public class FirstMissingPositive {
 
-	/**
-	 * @param args
-	 */
-	public static int firstMissingPositive(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int[] a = new int[A.length+1];
-        for(int i=0;i<A.length;i++){
-        	int value = A[i];
-        	if(value>0 && value<a.length) a[value]=value;
-        }
-        for(int i=1;i<a.length;i++){
-        	if(a[i]!=i) return i;
-        }
-        return a.length;
-    }
-	
-	
-	public static int firstMissingPositive1(int[] A) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int n = A.length;
-        int taken;
-        for(int i=0;i<n;i++){
-        	taken = A[i];
-        	while(taken>0){
-        		if(taken>=n){
-        			A[i] = -1;
-        			break;
-        		}
-        		int replace_i = taken;
-        		taken = A[taken];
-        		A[replace_i] = replace_i;
-        		A[i] = -1;
-        	}
-        }
-        for(int i=1;i<n;i++){
-        	if(A[i]==-1)
-        		return i;
-        }
-        return 1;
-    }
-	public static void p(Object o){
-		System.out.println(o);
+	public int firstMissingPositive(int[] A) {
+		if (A == null || A.length == 0) {
+			return 1;
+		}
+
+		int n = A.length;
+		int i = 0;
+		while (i < n) {
+			int val = A[i];
+			if (val > 0 && val <= n && i + 1 != val && val != A[val - 1]) {
+				swap(A, i, val - 1);
+			} else {
+				i++;
+			}
+		}
+
+		for (i = 0; i < n; i++) {
+			if (A[i] != i + 1) {
+				return i + 1;
+			}
+		}
+		return n + 1;
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] A = new int[]{1,2,3,5};
-		p(firstMissingPositive(A));
+
+	private void swap(int[] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
 	}
 
 }
