dcafdd899338a48527c399a7aa3b59ac11bcc2fc
Wenfei Liang <nebulaliang@gmail.com>
2015-01-29 21:58:54 -0500

    update 2015/01/29

--- src/grayCode/GrayCode.java
+++ src/grayCode/GrayCode.java
@@ -1,37 +1,25 @@
 package grayCode;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GrayCode {
 
-	/**
-	 * @param args
-	 */
-	public ArrayList<Integer> grayCode(int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		result.add(new Integer(0));
-		if(n==0) return result;
-		for(int i=0;i<n;i++){
-			result = getNext(result,i);
-		}
-        return result;
-    }
-	public static ArrayList<Integer> getNext(ArrayList<Integer> list,int n){
-		ArrayList<Integer> toAdd = new ArrayList<Integer>();
-		Integer adder = new Integer((int)(Math.pow(2, n)));
-		for(int i=0;i<list.size();i++){
-			toAdd.add(list.get(i)+adder);
-		}
-		for(int i=toAdd.size()-1;i>=0;i--){
-			list.add(toAdd.get(i));
-		}
-		return list;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<Integer>();
+        res.add(0);
+        if (n == 0) {
+            return res;
+        }
+        for (int i = 0; i < n; i++) {
+            int len = res.size();
+            for (int j = len - 1; j >= 0; j--) {
+                int add = (int) Math.pow(2, i);
+                res.add(res.get(j) + add);
+            }
+        }
 
-	}
+        return res;
+    }
 
 }
