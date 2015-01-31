75117e6635c2d1e40617ad9559e7f3f65884091a
Wenfei Liang <nebulaliang@gmail.com>
2015-01-31 15:29:15 -0500

    update 2015/01/31

--- src/pow/Pow.java
+++ src/pow/Pow.java
@@ -2,26 +2,27 @@ package pow;
 
 public class Pow {
 
-	/**
-	 * @param args
-	 */
-	public double pow(double x, int n) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		boolean neg = false;
-        if(n==0) return 1;
-        if(n<0) neg = true;
-        n = Math.abs(n);
-        double value = x;
-        for(int i=1;i<n;i++){
-        	value*=x;
+    public double pow(double x, int n) {
+        if (n == 0) {
+            return 1;
         }
-        if(neg) return 1/value;
-        return value;
+        if (n == 1) {
+            return x;
+        }
+        boolean neg = false;
+        if (n < 0) {
+            neg = true;
+            n = -n;
+        }
+        int k = n / 2;
+        int l = n - k * 2;
+        double t1 = pow(x, k);
+        double t2 = pow(x, l);
+        if (!neg) {
+            return t1 * t1 * t2;
+        } else {
+            return 1.0 / (t1 * t1 * t2);
+        }
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
