5473b4986399c0be9d49a4178b68958c8f6b930c
Wenfei Liang <nebulaliang@gmail.com>
2015-02-05 01:00:05 -0500

    update 2015/02/05

--- src/lengthOfLastWord/LengthOfLastWord.java
+++ src/lengthOfLastWord/LengthOfLastWord.java
@@ -2,28 +2,23 @@ package lengthOfLastWord;
 
 public class LengthOfLastWord {
 
-	/**
-	 * @param args
-	 */
-	public int lengthOfLastWord(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if(s.equals("")) return 0;
+    public int lengthOfLastWord(String s) {
+        if (s.equals("")) {
+            return 0;
+        }
+
         int n = s.length();
         int count = 0;
-        for(int i=n-1;i>=0;i--){
-        	if(s.charAt(i)!=' '){
-        		count++;
-        	}
-        	else{
-        		if(count!=0) return count;
-        	}
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                if (count != 0) {
+                    return count;
+                }
+            }
         }
         return count;
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
