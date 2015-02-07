f418511713a02ea8c6b56f18a5b41eaf0e655d23
Wenfei Liang <nebulaliang@gmail.com>
2015-02-06 20:21:15 -0500

    update 2015/02/06

--- src/maxAndminDepth/MaxDepth.java
+++ src/maxAndminDepth/MaxDepth.java
@@ -1,14 +1,18 @@
 package maxAndminDepth;
 
+import util.TreeNode;
+
 public class MaxDepth {
 
-	/**
-	 * @param args
-	 */
-	public int maxDepth(TreeNode root) {
-        if(root==null) return 0;
-        if(root.left==null && root.right==null) return 1;
-        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
     }
 
 }
--- src/maxAndminDepth/MinDepth.java
+++ src/maxAndminDepth/MinDepth.java
@@ -1,15 +1,24 @@
 package maxAndminDepth;
 
+import util.TreeNode;
+
 public class MinDepth {
 
-	/**
-	 * @param args
-	 */
-	 public int minDepth(TreeNode root) {
-		 	if(root==null) return 0;
-	        if(root.left==null) return minDepth(root.right)+1;
-	        else if(root.right==null) return minDepth(root.left)+1;
-	        else return Math.min(minDepth(root.left),minDepth(root.right))+1;
-	    }
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (left == 0) {
+            return right + 1;
+        }
+        if (right == 0) {
+            return left + 1;
+        }
+        return Math.min(left, right) + 1;
+    }
 
 }
