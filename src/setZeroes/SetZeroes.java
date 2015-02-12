f4640f8ab06c70517370a0b13cc6f27d6d5f1c8c
Wenfei Liang <wliang@roamdata.com>
2015-02-12 00:04:37 -0500

    update 2015/02/12

--- src/setZeroes/SetZeroes.java
+++ src/setZeroes/SetZeroes.java
@@ -2,36 +2,82 @@ package setZeroes;
 
 public class SetZeroes {
 
-	/**
-	 * @param args
-	 */
-	//EASY
-	//only one thing need to note: we can not replace any element at the first traverse
-	//because it would effect the following result.
-	public void setZeroes(int[][] matrix) {
-		int m = matrix.length;
-		int n = matrix[0].length;
-		boolean[] line = new boolean[m];
-		boolean[] col = new boolean[n];
-        for(int i=0;i<m;i++){
-        	for(int j=0;j<n;j++){
-        		if(matrix[i][j]==0){
-        			line[i] = true;
-        			col[j] = true;
-        		}
-        	}
+    /**
+     * @param args
+     */
+    // EASY
+    // only one thing need to note: we can not replace any element at the first
+    // traverse
+    // because it would effect the following result.
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] line = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    line[i] = true;
+                    col[j] = true;
+                }
+            }
         }
-        for(int i=0;i<m;i++){
-        	for(int j=0;j<n;j++){
-        		if(line[i] || col[j]){
-        			matrix[i][j]=0;
-        		}
-        	}
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (line[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    //inplace method
+    public void setZeroes1(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean row0 = false;
+        boolean col0 = false;
+        
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+            }
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (row0) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if (col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 
 }
