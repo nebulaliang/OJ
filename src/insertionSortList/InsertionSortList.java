a11e2dfdca6b57853b8abd30b88033d17663982e
Wenfei Liang <wliang@roamdata.com>
2015-02-19 23:10:21 -0500

    update 2015/02/19

---
+++ src/insertionSortList/InsertionSortList.java
@@ -0,0 +1,25 @@
+package insertionSortList;
+
+import util.ListNode;
+
+public class InsertionSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+}
