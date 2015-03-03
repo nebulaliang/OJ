2693f86105355244aaf0441e97dab2d597f2bab1
Wenfei Liang <wliang@roamdata.com>
2015-03-02 21:29:47 -0500

    update 2015/03/02

--- src/maxProfit/MaxProfit2.java
+++ src/maxProfit/MaxProfit2.java
@@ -2,27 +2,21 @@ package maxProfit;
 
 public class MaxProfit2 {
 
-	/**
-	 * @param args
-	 */
-	public int maxProfit(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int result = 0;
-        if(prices.length<2) return 0;
-        int i = 0;
-        int j = 1;
-        for(;i<prices.length-1;i++){
-        	if(prices[j]>prices[i]) {
-        		result+=prices[j]-prices[i];}
-        		j++;
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
         }
-        return result; 
+        
+        int take = prices[0];
+        int result = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if (prices[i] > take) {
+                result += prices[i] - take;
+            }
+            take = prices[i];    
+        } 
+        
+        return result;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
