5b50efa331559bdc3aa68a76e2f83f699b65e4e5
Wenfei Liang <wliang@roamdata.com>
2015-03-03 19:15:09 -0500

    update 2015/03/03

--- src/maxProfit/MaxProfit3.java
+++ src/maxProfit/MaxProfit3.java
@@ -2,49 +2,30 @@ package maxProfit;
 
 public class MaxProfit3 {
 
-	/**
-	 * @param args
-	 */
-	public static void p(Object o){
-		System.out.println(o);
-	}
-	public static int maxProfit(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		int result = 0;
-		int n = prices.length;
-        if(n<2) return 0;
-        int[] leftprofit = new int[n];
-        int[] rightprofit = new int[n];
-        leftprofit[0] = 0;
-        int small = prices[0];
-        for(int i=1;i<n;i++){
-        	int num = prices[i];
-        	if(num<small){
-        		small = num;
-        		leftprofit[i]=leftprofit[i-1];
-        	}
-        	else{
-        		int diff = num-small;
-        		if(diff>leftprofit[i-1]) leftprofit[i] = diff;
-        	}
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
         }
-        rightprofit[n-1] = 0;
-        small = prices[n-1];
-        int largeIndex = n-1;
-        for(int j=n-2;j>=0;j--){
-        	int num = prices[j];
-        	if(num>prices[largeIndex]) largeIndex = j;
-        	rightprofit[j] = Math.max(prices[largeIndex]-prices[j], rightprofit[j+1]);
-        	if(leftprofit[j]+rightprofit[j]>result) result = leftprofit[j]+rightprofit[j];
+        int len = prices.length;
+        int min = prices[0];
+        int[] left_maxProfit = new int[len];
+        //compute the left profit. Store each value at left_maxProfit
+        for (int i = 1; i < len; i++) {
+            left_maxProfit[i] = Math.max(left_maxProfit[i - 1], prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        return result;
+        //compute the right profit.
+        int max = prices[len - 1];
+        int right_maxProfit = 0;
+        int total_profit = left_maxProfit[len - 1];
+        
+        for (int i = len - 2; i >= 0; i--) {
+            right_maxProfit = Math.max(right_maxProfit, max - prices[i]);
+            max = Math.max(max, prices[i]);
+            total_profit = Math.max(total_profit, left_maxProfit[i] + right_maxProfit);
+        }
+        
+        return total_profit;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a = {1,2,4,2,5,7,2,4,9,0,9};
-		p(maxProfit(a));
-	}
 
 }
