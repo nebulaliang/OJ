57f3c08ea82c378f54078aa634b72cf43e56c86a
Wenfei Liang <wliang@roamdata.com>
2015-03-04 20:06:13 -0500

    update 2015/03/04

--- src/levelOrder/LevelOrder.java
+++ src/levelOrder/LevelOrder.java
@@ -1,44 +1,40 @@
 package levelOrder;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import util.TreeNode;
 
 public class LevelOrder {
 
-	/**
-	 * @param args
-	 */
-	
-	public static ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if(root==null) return result;
-		ArrayList<Integer> first_level = new ArrayList<Integer>();
-		first_level.add(new Integer(root.val));
-		result.add(first_level);
-		ArrayList<TreeNode> work_level = new ArrayList<TreeNode>();
-		work_level.add(root);
-		while(!work_level.isEmpty()){
-			ArrayList<TreeNode> next_level = new ArrayList<TreeNode>();
-			for(TreeNode t:work_level){
-				if(t.left!=null) next_level.add(t.left);
-				if(t.right!=null) next_level.add(t.right);
-			}
-			if(!next_level.isEmpty()){
-				ArrayList<Integer> next_integers = new ArrayList<Integer>();
-				for(TreeNode v:next_level){
-					next_integers.add(v.val);
-				}
-				result.add(next_integers);
-			}
-			work_level = next_level;
-		}
-		return result;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result   = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp  = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(temp);
+        }
+        
+        return result;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
