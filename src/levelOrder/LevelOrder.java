10b8108805812281acd12de6b858bb291008a4e5
Wenfei Liang <wliang@roamdata.com>
2015-03-05 20:13:33 -0500

    update 2015/03/05

--- src/levelOrder/LevelOrder.java
+++ src/levelOrder/LevelOrder.java
@@ -36,5 +36,34 @@ public class LevelOrder {
         
         return result;
     }
+    
+    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result   = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp  = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(0, temp);
+        }
+        
+        return result;
+    }
 
 }
