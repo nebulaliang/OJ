898c7f0c3d63593ee0cc983fa85e620e55f57881
Wenfei Liang <wliang@roamdata.com>
2015-03-06 22:37:30 -0500

    update 2015/03/06

--- src/levelOrder/LevelOrder.java
+++ src/levelOrder/LevelOrder.java
@@ -65,5 +65,39 @@ public class LevelOrder {
         
         return result;
     }
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result   = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int level = 1;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp  = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (level % 2 != 0) {
+                    temp.add(node.val);
+                } else {
+                    temp.add(0, node.val);
+                }
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(temp);
+            level++;
+        }
+        
+        return result;
+    }
 
 }
