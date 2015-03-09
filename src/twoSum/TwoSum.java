225f9eff7f398ab0811a4ec4c8db02ea99e54c97
Wenfei Liang <wliang@roamdata.com>
2015-03-09 19:56:28 -0400

    update 2015/03/09

--- src/twoSum/TwoSum.java
+++ src/twoSum/TwoSum.java
@@ -1,36 +1,32 @@
 package twoSum;
 
 import java.util.HashMap;
+
 public class TwoSum {
 
-	/**
-	 * @param args
-	 */
-	public int[] twoSum(int[] numbers, int target) {
-		//define a hashmap to store the history number in numbers(key is number,value is the index);
-        //go through number(n) in numbers, check whether there is number in hash x s.t. x = target - n;
-        //if yes, return the two indices
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int i=0;i<numbers.length;i++){
-            int cur_index = i+1;
+    public int[] twoSum(int[] numbers, int target) {
+        // define a hashmap to store the history number in numbers(key is
+        // number,value is the index);
+        // go through number(n) in numbers, check whether there is number in
+        // hash x s.t. x = target - n;
+        // if yes, return the two indices
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            int cur_index = i + 1;
             Integer num = new Integer(numbers[i]);
             Integer diff = new Integer(target - num);
-            //check whether there is number x in hash s.t. x = target - n;
-            if(map.containsKey(diff)){
+            // check whether there is number x in hash s.t. x = target - n;
+            if (map.containsKey(diff)) {
                 int index = map.get(diff).intValue();
-                return new int[]{index,cur_index};
+                return new int[] { index, cur_index };
             }
-            //if the hash has same key, do nothing
-            if(!map.containsKey(num)){
-                map.put(num,cur_index);
+            // if the hash has same key, do nothing
+            if (!map.containsKey(num)) {
+                map.put(num, cur_index);
             }
-            
+
         }
         return null;
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
