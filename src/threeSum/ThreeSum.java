6f5f235f59cbfc3f58edeae7e326265657011721
Wenfei Liang <wliang@roamdata.com>
2015-03-12 22:58:11 -0400

    update 2015/03/12

--- src/threeSum/ThreeSum.java
+++ src/threeSum/ThreeSum.java
@@ -1,75 +1,60 @@
 package threeSum;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 
 public class ThreeSum {
 
-	/**
-	 * @param args
-	 */
-	public static void p(Object o){
-		System.out.println(o);
-	}
-	public static ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-		int n = num.length;
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if(n<3){
-			return result;
-		}
-        Arrays.sort(num);
-        int mark = num[0]-1;
-        for(int i=0;i<n;i++){
-        	int first = num[i];
-        	if(mark==first){continue;}
-        	int target = 0-first;
-        	int j = i+1;
-        	int k = n-1;
-        	int mark_j = num[0]-1;
-			int mark_k = num[0]-1;
-        	while(j<k){
-        		if(num[j]+num[k]>target){
-        			k--;
-        		}
-        		else if(num[j]+num[k]<target){
-        			j++;
-        		}
-        		else{
-        			if(mark_j==num[j] && j<k){
-        				j++;
-        				continue;
-        			}
-        			if(mark_k==num[k] && j<k){
-        				k--;
-        				continue;
-        			}
-        			ArrayList<Integer> temp = new ArrayList<Integer>();
-        			temp.add(new Integer(num[i]));
-        			temp.add(new Integer(num[j]));
-        			temp.add(new Integer(num[k]));
-        			result.add(temp);
-        			mark_j=num[j];
-        			mark_k=num[k];
-        			j++;
-        			k--;
-        			mark = first;
-        		}
-        	}
+    public List<List<Integer>> threeSum(int[] num) {
+        int n = num.length;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (n < 3) {
+            return result;
         }
+        Arrays.sort(num);
+        int last1 = num[0] - 1;
+        List<Integer> last = new ArrayList<Integer>();
+        for (int i = 0; i <= n - 3; i++) {
+            int val = num[i];
+            if (val == last1) {
+                continue;
+            }
+            int target = -val;
+            int j = i + 1;
+            int k = n - 1;
+            int last2 = num[0] - 1;
+            int last3 = num[0] - 1;
+            while (j < k) {
+                int sum = num[j] + num[k];
+                if (sum == target) {
+                    if (last2 == num[j]) {
+                        j++;
+                        continue;
+                    }
+                    if (last3 == num[k]) {
+                        k--;
+                        continue;
+                    }
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(val);
+                    list.add(num[j]);
+                    list.add(num[k]);
+                    result.add(list);
+                    last1 = val;
+                    last2 = num[j];
+                    last3 = num[k];
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
         return result;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] num = {0,0,0,0};
-		ArrayList<ArrayList<Integer>> result = threeSum(num);
-		for(ArrayList<Integer> i:result){
-			
-			for(Integer n:i ){
-				System.out.print(n);System.out.print(" ");
-			}
-			p("");
-		}
-	}
 
 }
