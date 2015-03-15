2b13058518f19e190d3b62b80236600de4d79d9c
Wenfei Liang <wliang@roamdata.com>
2015-03-15 10:45:11 -0400

    update 2015/03/15

--- src/strStr/StrStr.java
+++ src/strStr/StrStr.java
@@ -2,52 +2,27 @@ package strStr;
 
 public class StrStr {
 
-	/**
-	 * @param args
-	 */
-	public String strStr(String haystack, String needle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		if (needle.length() == 0) {//you can always find an empty string
-            return haystack;
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) {
+            return -1;
         }
-        if (haystack.length () < needle.length()) {
-            return null;
-        }
-        int i = 0, j = 0;
-        while(i < haystack.length() && j < needle.length()) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-                if (j == needle.length()) {
-                    return haystack.substring(i - j);
+        
+        int h_len = haystack.length();
+        int n_len = needle.length();
+        
+        for (int i = 0; i < h_len - n_len + 1; i++) {
+            int j = 0;
+            for (; j < n_len; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
                 }
             }
-            else {
-                i = i - j + 1;
-                j = 0;
+            if(j == n_len) {
+                return i;
             }
         }
-        return null;
+        
+        return -1;
     }
-	public String search(String haystack, String needle){
-		for(int i=0;i<haystack.length();i++){
-			for(int j=0;j<needle.length() && (i+j)<haystack.length();j++){
-				if(needle.charAt(j)!=haystack.charAt(i+j)){
-					break;
-				}
-				if(j==needle.length()-1) return haystack.substring(i);
-			}
-		}
-		return null;
-	}
-	public static void p(Object o){
-		System.out.println(o);
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s = "12345";
-		p(s.substring(2));
-	}
 
 }
