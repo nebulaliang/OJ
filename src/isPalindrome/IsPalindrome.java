b1292ea4811f06aebe1f95dd9f82d1a6ad6002d8
Wenfei Liang <wliang@roamdata.com>
2015-03-19 21:09:05 -0400

    update 2015/03/19

--- src/isPalindrome/IsPalindrome.java
+++ src/isPalindrome/IsPalindrome.java
@@ -2,17 +2,44 @@ package isPalindrome;
 
 public class IsPalindrome {
 
-	/**
-	 * @param args
-	 */
-	public boolean isPalindrome(int x) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        
-    }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        if (s.length() == 1) {
+            return true;
+        }
 
-	}
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !isAlpha(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !isAlpha(s.charAt(j))) {
+                j--;
+            }
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlpha(char c) {
+        if (c >= '0' && c <= '9') {
+            return true;
+        }
+        if (c >= 'a' && c <= 'z') {
+            return true;
+        }
+        return false;
+    }
 
 }
